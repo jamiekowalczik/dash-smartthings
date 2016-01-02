c60815e6816e85e7d72e9d7fbe8a7e8ceb3163c8
Kowalczik <jamie.kowalczik@gmail.com>
2016-01-01 20:57:37 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# dash-smartthings
+
+# Dash Button IP Address
+# Obtain the MAC address of the Dash Button and
+# create a DHCP reservation for it on your DHCP Server/router
+192.168.2.137
+
+# Server running the xinetd dashproxy service
+192.168.2.100
+
+# IP Address to conifgure DDWRT router to forward traffic
+# from one dash button to the server
+/usr/sbin/iptables -t nat -A PREROUTING -s 192.168.2.137 -p tcp -j DNAT --to 192.168.2.100:9999
+
+# Using the SmartThings web IDE create a SmartApp using the groovy file.
+# Create a device in the simulator to obtain an API Token and API Endpoint address
+# and add them to the dashproxy.py file
---
+++ dashproxy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+## DD-WRT IPTABLES COMMAND (assumptions: dash button's ip is 192.168.2.137 and the server running the
+## xinetd service has an ip of 192.168.2.100
+## /usr/sbin/iptables -t nat -A PREROUTING -s 192.168.2.137 -p tcp -j DNAT --to 192.168.2.100:9999
+
+import requests
+import time
+from datetime import datetime
+
+timeFile = "/usr/local/bin/lastdashpush.txt"
+
+def toggle_st():
+    APITOKEN = "11111111-2222-3333-4444-555555555555"
+    API_ENDPOINT = "https://graph.api.smartthings.com/api/smartapps/installations/66666666-7777-8888-9999-000000000000"
+
+    ENDPOINTSUFFIX = "switches"
+    url = "%s/%s" % (API_ENDPOINT,ENDPOINTSUFFIX)
+    print url
+    headers = {"Authorization": "Bearer %s" % APITOKEN}
+    data = '{"command":"toggle"}'
+    r = requests.put(url, data=data, headers=headers)
+
+f = open(timeFile, 'r')
+lastPushTime = f.readline()
+f.close
+print lastPushTime
+
+timeFormat = "%H:%M:%S"
+curTime = time.strftime(timeFormat)
+print curTime
+
+tdelta = datetime.strptime(curTime, timeFormat) - datetime.strptime(lastPushTime, timeFormat)
+print tdelta
+
+if tdelta.seconds > 10:
+   f = open(timeFile, 'w')
+   f.write(curTime)
+   f.close
+
+   toggle_st()
+else:
+   print "Wait a bit"
+
